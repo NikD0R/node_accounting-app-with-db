4005f05e2411ac4bb6642255982b5eecf9b4a8c2
Nikita Doroshenko <nikitokd05@gmail.com>
2025-12-08 00:34:56 +0200

    task solution

--- src/db.js
+++ src/db.js
@@ -7,13 +7,13 @@ const utils = require('util');
 require('dotenv').config();
 global.TextEncoder = utils.TextEncoder;
 
-const {
-  POSTGRES_HOST,
-  POSTGRES_PORT,
-  POSTGRES_USER,
-  POSTGRES_PASSWORD,
-  POSTGRES_DB,
-} = process.env;
+// const {
+//   POSTGRES_HOST,
+//   POSTGRES_PORT,
+//   POSTGRES_USER,
+//   POSTGRES_PASSWORD,
+//   POSTGRES_DB,
+// } = process.env;
 
 /*
   All credentials setted to default values (exsept password - it is exapmle)
@@ -21,12 +21,12 @@ const {
 */
 
 const sequelize = new Sequelize({
-  database: POSTGRES_DB || 'postgres',
-  username: POSTGRES_USER || 'postgres',
-  host: POSTGRES_HOST || 'localhost',
+  database: 'postgres',
+  username: 'postgres',
+  host: 'localhost',
   dialect: 'postgres',
-  port: POSTGRES_PORT || 5432,
-  password: POSTGRES_PASSWORD || '123',
+  port: 5432,
+  password: '12ND1205',
 });
 
 module.exports = {
